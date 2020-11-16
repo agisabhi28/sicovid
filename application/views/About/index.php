98ccb6e575288a45303439ffa9cf0c10b6717dcf
agisabhi28 <agisabhi28@email.unikom.ac.id>
2020-11-16 09:38:01 +0700

    Views About

---
+++ application/views/About/index.php
@@ -0,0 +1,139 @@
+<div class="container">
+
+	<tr align="center">
+		<h1 class="mt-3">
+			<center>About
+		</h1>
+		</center>
+		<p>
+			Sistem Informasi ini dibuat sebagai tugas UAS Mata Kuliah Tugas Proyek E-Commerce. Kelompok kami mengusung tema Kesehatan. Anggota dari kelompok kami adalah sebagai berikut.
+		</p>
+		<table border="1" align="center">
+			<tr>
+				<td colspan="2" align="center">
+					<style>
+						img {
+							border-radius: 50%;
+						}
+					</style>
+					<img src="<?php echo base_url(); ?>assets/foto/euis.jpeg" width="200">
+					<br>
+					NAMA :<?php echo $nama; ?>
+					<br>
+					NIM :<?php echo $nim; ?>
+					<br>
+					JURUSAN :<?php echo $jurusan; ?>
+					<br>
+					FAKULTAS :<?php echo $fakultas; ?>
+				</td>
+			</tr>
+			<tr>
+
+				<td align="center" width="10%">
+					<style>
+						img {
+							border-radius: 50%;
+						}
+					</style>
+					<img src="<?php echo base_url(); ?>assets/foto/wawang.jpeg" width="200">
+					<br>
+					NAMA :<?php echo $nama4; ?>
+					<br>
+					NIM :<?php echo $nim4; ?>
+					<br>
+					JURUSAN :<?php echo $jurusan4; ?>
+					<br>
+					FAKULTAS :<?php echo $fakultas4; ?>
+
+				</td>
+
+				<td align="center" width="10%">
+					<style>
+						img {
+							border-radius: 50%;
+						}
+					</style>
+
+					<img src="<?php echo base_url(); ?>assets/foto/agisara.jpg" width="200">
+					<br>
+					NAMA :<?php echo $nama1; ?>
+					<br>
+					NIM :<?php echo $nim1; ?>
+					<br>
+					JURUSAN :<?php echo $jurusan1; ?>
+					<br>
+					FAKULTAS :<?php echo $fakultas1; ?>
+
+				</td>
+
+			</tr>
+
+
+			<tr>
+				<td align="center" width="10%">
+					<style>
+						img {
+							border-radius: 50%;
+						}
+					</style>
+
+					<img src="<?php echo base_url(); ?>assets/foto/jumaa.jpg" width="200">
+					<br>
+					NAMA :<?php echo $nama2; ?>
+					<br>
+					NIM :<?php echo $nim2; ?>
+					<br>
+					JURUSAN :<?php echo $jurusan2; ?>
+					<br>
+					FAKULTAS :<?php echo $fakultas2; ?>
+
+				</td>
+
+				<td align="center" width="10%">
+					<style>
+						img {
+							border-radius: 50%;
+						}
+					</style>
+
+					<img src="<?php echo base_url(); ?>assets/foto/hery.jpeg" width="200">
+					<br>
+					NAMA :<?php echo $nama3; ?>
+					<br>
+					NIM :<?php echo $nim3; ?>
+					<br>
+					JURUSAN :<?php echo $jurusan3; ?>
+					<br>
+					FAKULTAS :<?php echo $fakultas3; ?>
+				</td>
+			</tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</table>
+</div>
